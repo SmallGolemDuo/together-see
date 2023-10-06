cd323f0cfab8defeec7f2319b03d913a3ad9d89e
Woogie95 <choisw950928@gmail.com>
2023-10-06 15:08:12 +0900

    Initial commit

---
+++ src/main/java/com/smallgolemduo/togethersee/TogetherSeeApplication.java
@@ -0,0 +1,13 @@
+package com.smallgolemduo.togethersee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherSeeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TogetherSeeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/smallgolemduo/togethersee/TogetherSeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallgolemduo.togethersee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherSeeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
