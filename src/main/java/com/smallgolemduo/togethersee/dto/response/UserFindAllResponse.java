379ec2af471ba5e3898a6dc465d671e5cc88f88b
Woogie95 <choisw950928@gmail.com>
2023-10-10 18:32:09 +0900

    chore: 깃 충돌 해결 커밋

--- src/main/java/com/smallgolemduo/togethersee/dto/response/UserFindAllResponse.java
+++ src/main/java/com/smallgolemduo/togethersee/dto/response/UserFindAllResponse.java
@@ -3,7 +3,6 @@ package com.smallgolemduo.togethersee.dto.response;
 import com.smallgolemduo.togethersee.entity.User;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
