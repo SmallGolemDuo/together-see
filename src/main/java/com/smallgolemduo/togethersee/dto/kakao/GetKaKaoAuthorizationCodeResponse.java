400b5af1ebad2af8a40a13597fab089ca2d4ba95
Woogie95 <choisw950928@gmail.com>
2023-11-19 19:24:15 +0900

    feat: Kakao 로그인 코드 추가

---
+++ src/main/java/com/smallgolemduo/togethersee/dto/kakao/GetKaKaoAuthorizationCodeResponse.java
@@ -0,0 +1,18 @@
+package com.smallgolemduo.togethersee.dto.kakao;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class GetKaKaoAuthorizationCodeResponse {
+
+    private final String code;
+    private final String error;
+
+    public boolean error() {
+        return error != null;
+    }
+}
---
+++ src/main/java/com/smallgolemduo/togethersee/dto/kakao/GetKakaoTokenResponse.java
@@ -0,0 +1,20 @@
+package com.smallgolemduo.togethersee.dto.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetKakaoTokenResponse {
+
+    private static final int TOKEN_BODY_INDEX = 1;
+
+    @JsonProperty("id_token")
+    private String idToken;
+
+    public String getIdTokenBody() {
+        return idToken.split("\\.")[TOKEN_BODY_INDEX];
+    }
+}
